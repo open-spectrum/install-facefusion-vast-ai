ddf6e4db378bf2c6fd22642c5b872bbab01c2add
Raimundo Nonato The Spectrum <144294493+open-spectrum@users.noreply.github.com>
2025-09-20 20:21:01 -0300

    Create installation.sh

---
+++ installation.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+apt update -y
+apt upgrade -y
+apt install git-all -y
+apt install curl -y
+apt install ffmpeg -y
+
+sudo mkdir -p --mode=0755 /usr/share/keyrings
+curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | sudo tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+
+echo "deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main" | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+sudo apt-get update && sudo apt-get install cloudflared
+
+conda init --all
+conda create --name facefusion python=3.12 pip=25.0
+conda activate facefusion
+conda install -y nvidia/label/cuda-12.9.1::cuda-runtime nvidia/label/cudnn-9.10.0::cudnn 
+git clone https://github.com/open-spectrum/facefusion-nsfw-bypass.git
+cd facefusion-nsfw-bypass
+python install.py --onnxruntime cuda
+conda deactivate
+conda activate facefusion
+python facefusion.py run 
